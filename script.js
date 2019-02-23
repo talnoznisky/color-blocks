cab56073b5a6d1a77201d94eb7c9ff2d5fd67d8b
talnoznisky <tal.noznisky@gmail.com>
2019-02-22 23:51:15 -0600

    swatch index and new pages

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
 
-var colors = ["#E6A076", "#CC7F5A", "#406E85", "#0F4661", "#F9C850"]
+var colors = [
+["#E6A076", "#CC7F5A", "#406E85", "#0F4661", "#F9C850"],
+["#5F87A5", "#F7D8D9", "#F0BAC3", "#EAA1AC"],
+["#76CBBC", "#C2EAD2", "#FCFFD6", "#FFD5E6", "#F4AFCC"],
+["#529106", "#61A307", "#86B71B", "#B9BD00", "#8EA202", "#799203"]
+]
+
+
 var divs = document.getElementsByClassName("square")
 const container = document.getElementById("container")
-
-
-function getRandomColor(){
-return colors[Math.floor(Math.random() * Math.floor(5))]
+function getRandomColor(i){
+return colors[i][Math.floor(Math.random() * Math.floor(colors[i].length))]
 }
-
 function getRandomSquare(){
   return divs[Math.floor(Math.random() * Math.floor(divs.length))]
 }
-
 function randomColorReassignment(){
-  getRandomSquare().style.background = getRandomColor();
+  getRandomSquare().style.background = getRandomColor(i);
 }
-
 function onLoadColorSet(){
-  for(var i = 0; i < divs.length; i++){
-    divs[i].style.background = getRandomColor();
+  for(var j = 0; j < divs.length; j++){
+    divs[j].style.background = getRandomColor(i);
   }
 }
-
 function writeSquares(){
   for(var i = 0; i < 50; i++){
     const row = document.createElement('div');
     row.setAttribute('class', 'row container-fluid');;
     container.appendChild(row);
-
     for(var j = 0; j < 6; j++){
       const square = document.createElement('div');
       square.setAttribute('class', 'square col')
       row.appendChild(square);
+    }
+  }
+}
+function generateIndex(){
+  for(var j = 0; j < colors.length; j++){
+    const link = document.createElement('a')
+    const row = document.createElement('div');
+    link.setAttribute('href',`./docs/${j}.html`)
+    row.setAttribute('class', 'row container-fluid swatch-row');
+    container.appendChild(link);
+    link.appendChild(row)
+    for(var i = 0; i < colors[j].length; i++){
+      const swatch = document.createElement('div');
+      swatch.setAttribute('class','swatch col');
+      swatch.style.backgroundColor = colors[j][i];
+      row.appendChild(swatch)
     }
   }
 }
